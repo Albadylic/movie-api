91243e0a952f17b5634fe9f5f76eedb74c7c5ea5
Gregor <43413852+Albadylic@users.noreply.github.com>
2026-06-03 16:41:47 +0100

    server setup

---
+++ api/src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+app.use(express.json());
+
+const usersRouter = require("./routes/users");
+const moviesRouter = require("./routes/movies");
+const reviewsRouter = require("./routes/reviews");
+
+app.use("/users", usersRouter);
+app.use("/movies", moviesRouter);
+app.use("/reviews", reviewsRouter);
+
+// Error handler
+//  * this mounts after every other route
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: "Something went wrong" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`API running on port: ${PORT}`);
+});
